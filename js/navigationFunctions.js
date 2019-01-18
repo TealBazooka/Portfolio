6bd91943a8073cce4d3223a289c2df03eb53e980
Unknown <coryslaught@gmail.com>
2019-01-17 21:49:11 -0800

    Huge Javascript update
    -hamburger menu
    -

---
+++ js/navigationFunctions.js
@@ -0,0 +1,13 @@
+$(".nav-link").on("click", function() {
+    $('.collapse').collapse('hide');
+});
+
+// toggle hamburger menu
+$("#bars").on("click", function() {
+    if ($(this).attr("class") === "fas fa-bars") {
+        $(this).attr("class", "fas fa-times");
+    }
+    else {
+        $(this).attr("class", "fas fa-bars");
+    }
+});
